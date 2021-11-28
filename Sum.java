50442f10f7a26f09ad5ed36fa0b5364124fb2170
nito-garuda <nicolas.moragal@usm.cl>
2021-11-28 09:03:49 -0300

    Morse no terminado

--- Sum.java
+++ Sum.java
@@ -5,12 +5,6 @@ import java.io.FileNotFoundException;
 public class Sum{
 
     String[] listaLinea;
-    
-    private int[] convert(String[] strList){
-        int[] intList;
-        
-    }
-
     public int sumar(String archName){
         try{
             File numeros = new File(archName);
@@ -18,15 +12,26 @@ public class Sum{
             while(lector.hasNextLine()){
                 String linea = lector.nextLine();
                 listaLinea = linea.split(" ");
-                listaLinea
                 for(int i=0; i<10;i++){
-
+                    for(int o=i+1; o<10; o++){
+                        if(Integer.parseInt(listaLinea[i]) + Integer.parseInt(listaLinea[o])==65){
+                            System.out.println(listaLinea[i]+" + "+ listaLinea[o]+" = 65");
+                            return(1);
+                        }
+                    }
                 }
             }
         }
-        catch{
-
+        catch(FileNotFoundException e){
+            System.out.println("No se encontro archivo");
+            e.printStackTrace();
         }
+        return(0);
+    }
+
+    public static void main(String[] args){
+        Sum sumador = new Sum();
+        sumador.sumar("numeros.txt");
     }
 
 
